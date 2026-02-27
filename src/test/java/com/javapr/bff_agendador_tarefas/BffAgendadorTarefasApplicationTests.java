57a0bbfa4a4694e4db2006506130548f7bb85cfb
Pedro <pedrofbks@gmail.com>
2026-02-27 14:31:05 -0300

    commit iniicial

---
+++ src/test/java/com/javapr/bff_agendador_tarefas/BffAgendadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javapr.bff_agendador_tarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffAgendadorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

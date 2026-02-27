ef4f69f60279e1950916622707bcc937abe4bc2f
Pedro <pedrofbks@gmail.com>
2026-02-27 18:25:36 -0300

    save de emergencia

---
+++ src/main/java/com/javapr/bff_agendador_tarefas/business/dto/TarefasDTO.java
@@ -0,0 +1,28 @@
+package com.java.agendadortarefas.business.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.java.agendadortarefas.infrastructure.enums.StatusNotificacaoEnum;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.regex.Pattern;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TarefasDTO {
+
+    private String id;
+    private String nomeTarefa;
+    private String descricao;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime dataCriacao;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime dataEvento;
+    private String emailUsuario;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime dataAlteracao;
+    private StatusNotificacaoEnum statusNotificacaoEnum;
+}
